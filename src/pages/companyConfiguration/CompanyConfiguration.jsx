2069d4d506e61b18a52820ebeccd9a05a67be9a2
riyandifirman <riyandi.firman08@gmail.com>
2023-10-19 17:41:48 +0700

    finishing Main Company Configuration Page

--- src/pages/companyConfiguration/CompanyConfiguration.jsx
+++ src/pages/companyConfiguration/CompanyConfiguration.jsx
@@ -5,11 +5,26 @@ import FilterButton from '../../components/common/FilterButton/FilterButton'
 import SortButton from '../../components/common/SortButton/SortButton'
 import CountButton from '../../components/common/countButton/CountButton'
 import AddButton from '../../components/common/addButton/AddButton'
+import CompanyTable from './CompanyTable'
 
 const CompanyConfiguration = () => {
 
   return (
     <>
+      <div className="buttons-container">
+        <div className='left-buttons'>
+          <SearchBox className='search-box'/>
+          <FilterButton className='filter-button'/>
+          <SortButton className='sort-button'/>
+          <CountButton className='count-button'/>
+        </div>
+        <div className='right-buttons'>
+          <AddButton buttonText="Add Company"/>
+        </div>
+      </div>
+      <div className='company-table-container'>
+        <CompanyTable/>
+      </div>
     </>
   )
 }
