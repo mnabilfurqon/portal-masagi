c0cd44350dcf53b2f9f06a48aaf97c687f5ee1c3
riyandifirman <riyandi.firman08@gmail.com>
2023-10-20 17:43:05 +0700

    slicing detail company configuration

---
+++ src/pages/companyConfiguration/DetailCompanyConfiguration.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './detailCompanyConfiguration.css'
+import DetailCompanyTable from './DetailCompanyTable'
+
+const DetailCompanyConfiguration = () => {
+  return (
+    <>
+        <DetailCompanyTable />
+    </>
+  )
+}
+
+export default DetailCompanyConfiguration
