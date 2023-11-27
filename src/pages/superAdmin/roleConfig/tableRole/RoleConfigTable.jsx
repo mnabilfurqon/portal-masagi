d8ce3b80529b87dd11a8ac64cc3409c9489cc91d
bagussr <bagusssyamsu@gmail.com>
2023-11-27 16:27:54 +0700

    Implement add & revoke permission

--- src/pages/superAdmin/roleConfig/tableRole/RoleConfigTable.jsx
+++ src/pages/superAdmin/roleConfig/tableRole/RoleConfigTable.jsx
@@ -1,17 +1,17 @@
-import "./roleConfigTable.css";
-import React from "react";
-import { Modal, Table } from "antd";
-import { Button } from "antd";
-import { AiOutlineCheckCircle, AiOutlineFileSearch } from "react-icons/ai";
-import { BiTrash } from "react-icons/bi";
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { PiWarningCircleLight } from "react-icons/pi";
-import axios from "axios";
-import Cookies from "js-cookie";
+import './roleConfigTable.css';
+import React from 'react';
+import { Modal, Table } from 'antd';
+import { Button } from 'antd';
+import { AiOutlineCheckCircle, AiOutlineFileSearch } from 'react-icons/ai';
+import { BiTrash } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { PiWarningCircleLight } from 'react-icons/pi';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const RoleConfigTable = ({ searchValue, sortValue, countValue, modalOpen }) => {
-  const token = Cookies.get("token");
+  const token = Cookies.get('token');
   const navigate = useNavigate();
   const [uuid, setUuid] = useState(null);
   const [roleData, setRoleData] = useState([]);
@@ -24,7 +24,7 @@ const RoleConfigTable = ({ searchValue, sortValue, countValue, modalOpen }) => {
       current: 1,
       pageSize: countValue,
       showTotal: (total, range) => (
-        <div className="total-data">
+        <div className='total-data'>
           {range[0]}-{range[1]} of {total} items
         </div>
       ),
@@ -46,20 +46,17 @@ const RoleConfigTable = ({ searchValue, sortValue, countValue, modalOpen }) => {
         page = tableParams.pagination.current;
       }
       console.log(sortValue);
-      const response = await axios.get(
-        "https://attendance-1-r8738834.deta.app/api/v1/role/",
-        {
-          params: {
-            page: page,
-            per_page: countValue,
-            search: searchValue,
-            desc: sortValue === "latest" ? true : false,
-          },
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
+      const response = await axios.get('http://localhost:5000/api/v1/role/', {
+        params: {
+          page: page,
+          per_page: countValue,
+          search: searchValue,
+          desc: sortValue === 'latest' ? true : false,
+        },
+        headers: {
+          Authorization: token,
+        },
+      });
       setRoleData(response.data[0].items);
       setTableParams({
         ...tableParams,
@@ -98,32 +95,32 @@ const RoleConfigTable = ({ searchValue, sortValue, countValue, modalOpen }) => {
 
   useEffect(() => {
     if (!token) {
-      navigate("/login");
+      navigate('/login');
     }
     getRoleData();
   }, [token, navigate, modalOpen, params, countValue, searchValue, sortValue]);
 
-  const handleDetailClick = (record) => {
+  const handleDetailClick = record => {
     const value = record.key;
     navigate(`/role/detail-role/${value}`);
   };
 
-  const handleDeleteButton = (record) => {
+  const handleDeleteButton = record => {
     setDeleteModalOpen(true);
     setUuid(record.key);
   };
 
   const successTitle = (
-    <div className="success-title">
-      <AiOutlineCheckCircle size={80} className="success-logo" />
-      <p className="success-text">Success</p>
+    <div className='success-title'>
+      <AiOutlineCheckCircle size={80} className='success-logo' />
+      <p className='success-text'>Success</p>
     </div>
   );
 
   const deleteTitle = (
-    <div className="delete-title">
-      <PiWarningCircleLight size={70} className="caution-icon" />
-      <p className="delete-header">Attention</p>
+    <div className='delete-title'>
+      <PiWarningCircleLight size={70} className='caution-icon' />
+      <p className='delete-header'>Attention</p>
     </div>
   );
 
@@ -137,39 +134,37 @@ const RoleConfigTable = ({ searchValue, sortValue, countValue, modalOpen }) => {
 
   const title = [
     {
-      key: "roleName",
-      title: "Role Name",
-      dataIndex: "roleName",
+      key: 'roleName',
+      title: 'Role Name',
+      dataIndex: 'roleName',
     },
     {
-      key: "action",
-      title: <div className="action-title">Action</div>,
-      render: (record) => (
-        <div className="action-container">
+      key: 'action',
+      title: <div className='action-title'>Action</div>,
+      render: record => (
+        <div className='action-container'>
           <Button
-            className="detail-button"
-            type="primary"
+            className='detail-button'
+            type='primary'
             onClick={() => handleDetailClick(record)}
-            size="small"
-            ghost
-          >
-            <AiOutlineFileSearch className="detail-icon" />
+            size='small'
+            ghost>
+            <AiOutlineFileSearch className='detail-icon' />
           </Button>
           <Button
-            className="delete-button"
-            type="primary"
-            size="small"
+            className='delete-button'
+            type='primary'
+            size='small'
             onClick={() => handleDeleteButton(record)}
-            ghost
-          >
-            <BiTrash className="delete-role-icon" />
+            ghost>
+            <BiTrash className='delete-role-icon' />
           </Button>
         </div>
       ),
     },
   ];
 
-  const tabel_data = roleData.map((item) => {
+  const tabel_data = roleData.map(item => {
     return {
       key: item.uuid,
       roleName: item.name,
@@ -214,24 +209,21 @@ const RoleConfigTable = ({ searchValue, sortValue, countValue, modalOpen }) => {
         onCancel={handleCancelButton}
         footer={[
           <Button
-            type="text"
-            key="cancelButton"
-            className="cancel-delete-role"
-            onClick={handleCancelButton}
-          >
+            type='text'
+            key='cancelButton'
+            className='cancel-delete-role'
+            onClick={handleCancelButton}>
             CANCEL
           </Button>,
           <Button
-            key="deleteButton"
-            className="delete-role"
+            key='deleteButton'
+            className='delete-role'
             onClick={deleteRoleData}
-            loading={deleting}
-          >
+            loading={deleting}>
             DELETE
           </Button>,
-        ]}
-      >
-        <p className="delete-caption">Are you sure delete this role?</p>
+        ]}>
+        <p className='delete-caption'>Are you sure delete this role?</p>
       </Modal>
 
       <Modal
@@ -239,18 +231,16 @@ const RoleConfigTable = ({ searchValue, sortValue, countValue, modalOpen }) => {
         centered
         open={deleteSuccessModalOpen}
         onCancel={() => setDeleteSuccessModalOpen(false)}
-        footer={null}
-      >
-        <div className="modal-content">
-          <p className="success-caption">Delete successfull!</p>
+        footer={null}>
+        <div className='modal-content'>
+          <p className='success-caption'>Delete successfull!</p>
           <Button
-            key="successDeleteRole"
-            className="save-button"
+            key='successDeleteRole'
+            className='save-button'
             onClick={() => {
-              setDeleteSuccessModalOpen(false)
-              reloadTableData()
-            }}
-          >
+              setDeleteSuccessModalOpen(false);
+              reloadTableData();
+            }}>
             Ok
           </Button>
         </div>
